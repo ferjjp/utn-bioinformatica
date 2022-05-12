d737495c72d53494b006333fc107694e333346d1
ferjjp <fer.jjp@gmail.com>
2022-05-12 00:02:43 -0300

    add download.sh & reporte remoto

---
+++ download.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/sh
+
+# Correr desde el root del proyecto
+
+# Download swissprot db in fasta format
+wget https://ftp.ncbi.nlm.nih.gov/blast/db/FASTA/swissprot.gz
+gzip -d swissprot.gz 
+
+# Download blast+
+wget https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ncbi-blast-2.13.0+-x64-linux.tar.gz
+tar xvf ncbi-blast-2.13.0+-x64-linux.tar.gz
+
+BLASTPLUSDIR=$(pwd)/ncbi-blast-2.13.0+/bin ./Ex2_local.pm
