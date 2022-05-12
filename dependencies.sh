6c98b86beb5861132d0eed07e2815265a581b868
ferjjp <fer.jjp@gmail.com>
2022-05-12 00:12:19 -0300

    add dependencies.sh

---
+++ dependencies.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/sh 
+
+sudo cpan -f Bio::Procedural
+sudo cpan -f Bio::Perl
+sudo cpan -f Bio::SeqIO
+sudo cpan -f Bio::Seq
+sudo cpan -f Bio::DB::NCBIHelper
+sudo cpan -f Bio::DB::Query::GenBank
+sudo cpan -f Bio::Tools::Run::StandAloneBlastPlus
