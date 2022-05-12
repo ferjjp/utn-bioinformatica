9d81709ca4fa77d78e39df08cf0145ee21959f1a
ferjjp <fer.jjp@gmail.com>
2022-05-12 00:21:28 -0300

    Add skeleton for Ex3. Add comment to Ex2

--- Ex2.pm
+++ Ex2.pm
@@ -6,6 +6,8 @@ use Bio::SeqIO;
 #https://metacpan.org/pod/Bio::Perl
 my $seqs = Bio::SeqIO->new(-file => "variante2_orfs.fasta", -format => "fasta" );
 
+
+#https://blast.ncbi.nlm.nih.gov/Blast.cgi?PROGRAM=blastp&PAGE_TYPE=BlastSearch&LINK_LOC=blasthome
 while(my $seq = $seqs->next_seq()) {
     my $blast = blast_sequence($seq->seq);
     write_blast(">>reporte_variante_2_orfs_remoto.bls", $blast);
